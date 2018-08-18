66832b183bbb2f8592fec7dfdca1d94378146263
Patric Guifo <me@juntos.fr>
2018-08-18 10:32:11 +0200

    test library permission

--- Sample/src/main/java/andro/permission/sample/MainActivity.kt
+++ Sample/src/main/java/andro/permission/sample/MainActivity.kt
@@ -3,9 +3,10 @@ package andro.permission.sample
 import andro.permission.PermissionFactory
 import andro.permission.core.GrantResult
 import andro.permission.core.PermissionManager
-import andro.permission.sample.R
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,23 +15,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         permissionManager = PermissionFactory.createPermissionManager(this.applicationContext)
+        testCameraPermission()
+    }
 
-        val cameraPermission = permissionManager.manifestPermission().CameraPermission()
-        permissionManager.requestPermission(cameraPermission)
-                ?.subscribe(
-                        { grantResult -> onGrantSuccess(grantResult)},
-                        { error -> onError(error)}
-                )
+    private fun testCameraPermission(){
+        request.setOnClickListener {
+            val cameraPermission = permissionManager.manifestPermission().CameraPermission()
+            permissionManager.requestPermission(cameraPermission)
+                    ?.subscribe(
+                            { grantResult -> onGrantSuccess(grantResult)},
+                            { error -> onError(error)}
+                    )
+        }
     }
 
 
     private fun onGrantSuccess(grantResult: GrantResult){
-        grantResult.toString()
+        Toast.makeText(this, grantResult.permission +" : "+grantResult.granted+" : "+grantResult.shouldShowRequestPermission, Toast.LENGTH_LONG).show()
     }
 
     private fun onError(error: Throwable){
-        error.toString()
+        Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
     }
 }
